de1b1ca3e6970b2bcfb692bf9bb264073f6a0a23
Aaron Donelson <aaron.donelson23@gmail.com>
2021-09-20 08:44:31 -0400

    made previous search clickable and set local storage

--- script.js
+++ script.js
@@ -13,8 +13,9 @@ var searchFormEl = $('#search-form');
 var previousSpan = document.querySelector('#previousSearch');
 var displayInfoContainer = document.getElementById('displayInfo');
 
-
-
+var arrayName = JSON.parse(localStorage.getItem("name")) || [];
+console.log(arrayName);
+showHistory();
 
 fetch(requestUrl)
   //fetch is first calling for the URL the promising to wait until it is ready to
@@ -26,117 +27,109 @@ fetch(requestUrl)
 
     console.log(data);
   });
+  function display(cityName){
+    
+    var requestUrl = urlFront.concat(cityName, urlEnd)
 
+    // console.log(cityName);
+  
+    // console.log(requestUrl);
+    // This fetch statement takes the users input and returns the correct cities api 
+    fetch(requestUrl)
+  
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+  
+          var cityName = document.createElement('p');
+          var weatherIcon = document.createElement('img');
+          var mainTemp = document.createElement('p');
+          var mainHumidity = document.createElement('p');
+          var windSpeed = document.createElement('p');
+          var latitude = JSON.stringify(data.coord.lat);
+          var longitude = JSON.stringify(data.coord.lon);
+          var uvIndex = document.createElement('p');
+          displayInfoContainer.textContent="";
+  
+          var date = new Date();
+          var dateMonth = date.toLocaleDateString();
+          
+          var iconurl = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+          
+  
+          cityName.textContent = data.name + " " + dateMonth;
+          weatherIcon.setAttribute("src", iconurl);
+          mainTemp.textContent = "Tempature:" + " " + data.main.temp + "\xB0" + "F";
+          mainHumidity.textContent = "Humidity:" + " " + data.main.humidity + "%";
+          windSpeed.textContent = "Current Wind Speed:" + " " + data.wind.speed + "MPH";
+  
+          displayInfoContainer.append(cityName);
+          displayInfoContainer.append(weatherIcon);
+          displayInfoContainer.append(mainTemp);
+          displayInfoContainer.append(mainHumidity);
+          displayInfoContainer.append(windSpeed);
+  
+          latitude.textContent = data.coord.lat;
+          longitude.textContent = data.coord.lon;
+  
+          
+          console.log(latitude);
+          console.log(longitude);
+  
+          var requestUrl = urlFront.concat(urlLat, latitude, urlLon, longitude);
+  
+          console.log(data);
+          console.log(data.name);
+          console.log(data.weather[0].icon);
+          console.log(data.main.temp);
+          console.log(data.main.humidity);
+          console.log(data.wind.speed);
+  
+          fetch(requestUrl)
+  
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        
+        console.log(data); 
+            
+      });
+  
+  });
+  }
+
+  function showHistory() {
+    previousSearchEl.empty();
+    for (let index = 0; index < arrayName.length; index++) {
+      previousSearchEl.append('<li class="cityName">' + arrayName[index] + '</li>');     
+    }
+    
+    $('.cityName').on('click', function(){
+    display($(this).text())
+    })
+  }
 function handleFormSubmit(event) {
   event.preventDefault();
 
-  var pastSearch = $('input[name="searchBox"]').val();
+  
+  var cityName = $('input[name="searchBox"]').val();
+  arrayName.push(cityName);
+  localStorage.setItem("name", JSON.stringify(arrayName));
+  showHistory();
+ 
+
+  
+  
   // if there's nothing in the form entered, don't print to the page
-  if (!pastSearch) {
+  if (!cityName) {
     alert('No search item filled out in form!');
     return;
   }
-
-  previousSearchEl.append('<li>' + pastSearch + '</li>');
-
-  $('input[name="searchBox"]').val('');
-
-  var requestUrl = urlFront.concat(pastSearch, urlEnd)
-
-  // console.log(pastSearch);
-
-  // console.log(requestUrl);
-  // This fetch statement takes the users input and returns the correct cities api 
-  fetch(requestUrl)
-
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-
-      // for (var i = 0; i < data.length; i++) {
-
-        var cityName = document.createElement('p');
-        var weatherIcon = document.createElement('p');
-        var mainTemp = document.createElement('p');
-        var mainHumidity = document.createElement('p');
-        var windSpeed = document.createElement('p');
-        var latitude = JSON.stringify(data.coord.lat);
-        var longitude = JSON.stringify(data.coord.lon);
-        var uvIndex = document.createElement('p');
-
-        var date = new Date();
-        var dateMonth = date.toLocaleDateString();
-        
-
-        
-
-        cityName.textContent = data.name + " " + dateMonth;
-        weatherIcon.textContent = data.weather[0].icon;
-        mainTemp.textContent = "Tempature:" + " " + data.main.temp + "\xB0" + "F";
-        mainHumidity.textContent = "Humidity:" + " " + data.main.humidity + "%";
-        windSpeed.textContent = "Current Wind Speed:" + " " + data.wind.speed + "MPH";
-
-        displayInfoContainer.append(cityName);
-        displayInfoContainer.append(weatherIcon);
-        displayInfoContainer.append(mainTemp);
-        displayInfoContainer.append(mainHumidity);
-        displayInfoContainer.append(windSpeed);
-
-        latitude.textContent = data.coord.lat;
-        longitude.textContent = data.coord.lon;
-
-        // JSON.stringify(latitude);
-        // JSON.stringify(longitude);
-
-        console.log(latitude);
-        console.log(longitude);
-
-        var requestUrl = urlFront.concat(urlLat, latitude, urlLon, longitude);
-
-        console.log(data);
-        console.log(data.name);
-        console.log(data.weather[0].icon);
-        console.log(data.main.temp);
-        console.log(data.main.humidity);
-        console.log(data.wind.speed);
-
-        fetch(requestUrl)
-
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      
-      console.log(data); 
-        
-       
-
-        // for (var i = 0; i < data.length; i++){
-        //   console.log(data[i].name);
-        //   console.log(data[i].weather[0].icon);
-        //   console.log(data[i].main.temp);
-        //   console.log(data[i].main.humidity);
-        //   console.log(data[i].wind.speed);
-
-        // }
-      // }
-
-    });
-
-});
+  // $('input[name="searchBox"]').val('');
+  display(cityName);
 
 }
-
-// function handleSearch(){
-//   var cityWeather = localStorage.getItem('previousSearch');
-
-//   previousSpan.textContent = cityWeather;
-
-//   var span = document.querySelector('#searchBox').value;
-
-//   localStorage.setItem("previousSearch", span)
-
-// }  
 
 searchFormEl.on('submit', handleFormSubmit);
